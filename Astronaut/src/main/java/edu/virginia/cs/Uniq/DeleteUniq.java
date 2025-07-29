105d000d832b881edd083c41e6f1f1533ed6d5ae
Clay Stevens <cdsteven@iastate.edu>
2025-07-29 11:17:50 -0500

    adding some checks for already running docker stuff

--- Astronaut/src/main/java/edu/virginia/cs/Uniq/DeleteUniq.java
+++ Astronaut/src/main/java/edu/virginia/cs/Uniq/DeleteUniq.java
@@ -1,138 +1,135 @@
 package edu.virginia.cs.Uniq;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class DeleteUniq {
-	public static void del(String path) {
+    public static void del(String path) {
 
-		String dirPath = path;
-		HashSet<String> uniqFileList = uniqueFiles(cksums(getFileList(dirPath)));
-		
-		File folder = new File(dirPath);
+        String dirPath = path;
+        Set<String> uniqFileList = uniqueFiles(cksums(getFileList(dirPath)));
 
-		ArrayList<String> delFiles = new ArrayList<String>();
-		for (final File fileEntry : folder.listFiles()) {
-			String filePath = fileEntry.getAbsolutePath();
-			if (filePath.endsWith("xml") && !uniqFileList.contains(filePath)) {
-				delFiles.add(filePath);
-			}
-		}
-		
-		// delete xml files
-		for(String s : delFiles){
-			new File(s).delete();
-		}
+        File folder = new File(dirPath);
 
-	}
+        ArrayList<String> delFiles = new ArrayList<String>();
+        for (final File fileEntry : folder.listFiles()) {
+            String filePath = fileEntry.getAbsolutePath();
+            if (filePath.endsWith("xml") && !uniqFileList.contains(filePath)) {
+                delFiles.add(filePath);
+            }
+        }
 
-	// get file list by path
-	public static ArrayList<String> getFileList(String dir) {
-		ArrayList<String> lists = new ArrayList<String>();
-		File folder = new File(dir);
+        // delete xml files
+        for (String s : delFiles) {
+            new File(s).delete();
+        }
 
-		for (final File fileEntry : folder.listFiles()) {
-			String filePath = fileEntry.getAbsolutePath();
-			if (filePath.endsWith("xml")) {
-				lists.add(filePath);
-			}
-		}
-		return lists;
-	}
+    }
 
-	// get checksum of all files
-	public static ArrayList<Pair<String>> cksums(ArrayList<String> files) {
-		ArrayList<Pair<String>> cksums = new ArrayList<Pair<String>>();
-		for (String s : files) {
-			try {
-				Process p = Runtime.getRuntime().exec("cksum " + s);
-				p.waitFor();
+    // get file list by path
+    public static ArrayList<String> getFileList(String dir) {
+        ArrayList<String> lists = new ArrayList<String>();
+        File folder = new File(dir);
 
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(p.getInputStream()));
-				String firstLine = reader.readLine();
-				String[] splited = firstLine.split(" ");
-				cksums.add(new Pair<String>(splited[2], splited[0]));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return cksums;
-	}
+        for (final File fileEntry : folder.listFiles()) {
+            String filePath = fileEntry.getAbsolutePath();
+            if (filePath.endsWith("xml")) {
+                lists.add(filePath);
+            }
+        }
+        return lists;
+    }
 
-	public static HashSet<String> uniqueFiles(
-			ArrayList<Pair<String>> filesWithCksums) {
-		ArrayList<String> sums = new ArrayList<String>();
-		for (Pair<String> p : filesWithCksums) {
-			sums.add(p.getSecond());
-			//System.out.println(p.getSecond());
-		}
+    // get checksum of all files
+    public static List<Map.Entry<String, String>> cksums(List<String> files) {
+        var cksums = new ArrayList<Map.Entry<String, String>>();
+        for (String s : files) {
+            try {
+                Process p = Runtime.getRuntime().exec("cksum " + s);
+                p.waitFor();
 
-		Set<String> uniqueSums = new HashSet<String>(sums);
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(p.getInputStream()));
+                String firstLine = reader.readLine();
+                String[] splited = firstLine.split(" ");
+                cksums.add(Map.entry(splited[2], splited[0]));
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return cksums;
+    }
 
-		ArrayList<String> files = new ArrayList<String>();
-		// find out unique files with HashSet
-		HashSet<String> hashSet = new HashSet<String>();
+    public static Set<String> uniqueFiles(
+            List<Map.Entry<String, String>> filesWithCksums) {
+        var sums = new ArrayList<String>();
+        for (var p : filesWithCksums) {
+            sums.add(p.getValue());
+        }
 
-		for (String u : uniqueSums) {
-			for (Pair<String> p : filesWithCksums) {
-				if (p.getSecond().equals(u)) {
-					hashSet.add(p.getFirst());
-					break;
-				}
-			}
-		}
+        Set<String> uniqueSums = new HashSet<String>(sums);
 
-		return hashSet;
-	}
+        ArrayList<String> files = new ArrayList<String>();
+        // find out unique files with HashSet
+        HashSet<String> hashSet = new HashSet<String>();
 
-	// compare files by contains
-	public static ArrayList<String> compare(ArrayList<String> files) {
-		int filesSize = files.size();
-		// 0 for same, 1 for different
-		int [][] matrix = new int[filesSize][filesSize];
-		ArrayList<String> fileList = new ArrayList<String>();
-		HashMap<String, Integer> fileMap = new HashMap<String, Integer>();
-		Process p;
-		try {
-			for (int i = 0; i < files.size(); i++) {
-				for (int j = 0; j < files.size(); j++) {
-					String iFile = files.get(i);
-					String jFile = files.get(j);
-					p = Runtime.getRuntime()
-							.exec("diff " + iFile + " " + jFile);
-					p.waitFor();
+        for (String u : uniqueSums) {
+            for (var p : filesWithCksums) {
+                if (p.getValue().equals(u)) {
+                    hashSet.add(p.getKey());
+                    break;
+                }
+            }
+        }
 
-					BufferedReader reader = new BufferedReader(
-							new InputStreamReader(p.getInputStream()));
-					String firstLine = reader.readLine();
-					if (firstLine != null) {
-						matrix[i][j] = 1;
-					}
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        return hashSet;
+    }
 
-		// iterate map and copy to a list
-		Set<String> set = fileMap.keySet();
-		for (String s : set) {
-			fileList.add(s);
-		}
-		return fileList;
-	}
+    // compare files by contains
+    public static List<String> compare(List<String> files) {
+        int filesSize = files.size();
+        // 0 for same, 1 for different
+        int[][] matrix = new int[filesSize][filesSize];
+        List<String> fileList = new ArrayList<>();
+        Map<String, Integer> fileMap = new HashMap<>();
+        Process p;
+        try {
+            for (int i = 0; i < files.size(); i++) {
+                for (int j = 0; j < files.size(); j++) {
+                    String iFile = files.get(i);
+                    String jFile = files.get(j);
+                    p = Runtime.getRuntime()
+                            .exec("diff " + iFile + " " + jFile);
+                    p.waitFor();
+
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(p.getInputStream()));
+                    String firstLine = reader.readLine();
+                    if (firstLine != null) {
+                        matrix[i][j] = 1;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // iterate map and copy to a list
+        Set<String> set = fileMap.keySet();
+        for (String s : set) {
+            fileList.add(s);
+        }
+        return fileList;
+    }
 }
--- init_docker.sh
+++ init_docker.sh
@@ -1,32 +1,53 @@
 #!/usr/bin/env bash
 
-echo "Enter a password for the MySQL database: "
-read -s mysql_password
+NETWORK_NAME=astronaut-network
 
-# start the Spark network
-docker network create astronaut-network
+MYSQL_NAME=astronaut-mysql
+SPARK_NAME=astronaut-spark
+SNODE_NAME=astronaut-worker
 
-# start up the mysql container
-docker run -d --name astronaut-mysql \
-  --network astronaut-network \
-  -e ALLOW_EMPTY_PASSWORD=yes \
-  -e MYSQL_ROOT_PASSWORD=$mysql_password \
-  -v data:/bitnami/mysql/data \
-  bitnami/mysql:latest
+if docker network inspect $NETWORK_NAME >/dev/null 2>&1; then
+  echo "Astronaut network already exists, skipping..."
+else
+  docker network create $NETWORK_NAME
+fi
 
-# start the Spark network
-docker run -d --name astronaut-spark \
-  --network astronaut-network \
-  -e SPARK_MODE=master \
-  -p 8080:8080 \
-  -p 7077:7077 \
-  bitnami/spark:latest
+if docker ps | grep -q "$MYSQL_NAME"; then
+  echo "Astronaut database container up, skipping..."
+else
+  echo "Enter a password for the MySQL database: "
+  read -s mysql_password
 
-# start the Spark worker
-docker run -d --name astronaut-worker \
-  --network astronaut-network \
-  -e SPARK_MODE=worker \
-  -e SPARK_MASTER_URL=spark://astronaut-spark:7077 \
-  -p 8081:8081 \
-  bitnami/spark:latest
+  # start up the mysql container
+  docker run -d --name $MYSQL_NAME \
+    --network $NETWORK_NAME \
+    -e ALLOW_EMPTY_PASSWORD=yes \
+    -e MYSQL_ROOT_PASSWORD=$mysql_password \
+    -v data:/bitnami/mysql/data \
+    bitnami/mysql:latest
+fi
+  
+if docker ps | grep -q "$SPARK_NAME"; then
+  echo "Astronaut Spark cluster up, skipping..."
+else
+  # start the Spark network
+  docker run -d --name $SPARK_NAME \
+    --network $NETWORK_NAME \
+    -e SPARK_MODE=master \
+    -p 8080:8080 \
+    -p 7077:7077 \
+    bitnami/spark:latest
+fi
+
+if docker ps | grep -q "$SNODE_NAME"; then
+  echo "Astronaut worker up, skipping..."
+else
+  # start the Spark worker
+  docker run -d --name $SNODE_NAME \
+    --network $NETWORK_NAME \
+    -e SPARK_MODE=worker \
+    -e "SPARK_MASTER_URL=spark://$SPARK_NAME:7077" \
+    -p 8081:8081 \
+    bitnami/spark:latest
+fi
 
